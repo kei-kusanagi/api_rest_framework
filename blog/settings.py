e30c1480f7f12f0853e7105643a267741b6598b2
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-08-23 16:41:39 -0500

    add swagger

--- blog/settings.py
+++ blog/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'ap_blog',
     'rest_framework',
-    
 ]
 
 MIDDLEWARE = [
